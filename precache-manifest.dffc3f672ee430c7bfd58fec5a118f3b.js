dd7efae49ac3042652a65dd800c89fd3d7ffcd78
Dmitry Batsenko <batsenkod@gmail.com>
2019-06-07 12:56:45 +0300

    Updates

---
+++ precache-manifest.dffc3f672ee430c7bfd58fec5a118f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ad7b8bd886aef5fac1c867885ca7a3",
+    "url": "batsenko.github.io/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "a0276cdea7a9181712f0",
+    "url": "batsenko.github.io/react_static-list-of-posts/static/css/main.6e36fd17.chunk.css"
+  },
+  {
+    "revision": "94af7f8b07d7d4877212",
+    "url": "batsenko.github.io/react_static-list-of-posts/static/js/2.15fbe00d.chunk.js"
+  },
+  {
+    "revision": "a0276cdea7a9181712f0",
+    "url": "batsenko.github.io/react_static-list-of-posts/static/js/main.9148aaa2.chunk.js"
+  },
+  {
+    "revision": "88f752cd93eaa3050bd3",
+    "url": "batsenko.github.io/react_static-list-of-posts/static/js/runtime~main.32ef2405.js"
+  },
+  {
+    "revision": "49f98c8e3027a1c3b2b7a0d65b84a6a5",
+    "url": "batsenko.github.io/react_static-list-of-posts/static/media/logo.49f98c8e.svg"
+  }
+]);
